48f4b140583d91fbbaaffac34255c5c8ce949031
George Marselis <george.marselis@ncmm.uio.no>
2019-02-11 13:48:39 +0100

    fixing it up for my tastes

--- rt-notify.py
+++ rt-notify.py
@@ -11,11 +11,28 @@ import subprocess
 import re
 import time
 
-QUERY = "\"Owner=\'Nobody\' AND ( Status=\'new\' OR Status=\'open\' ) AND ( Queue!=\'spambin\' AND Queue!=\'maildrop\' AND Queue!=\'learnspam\' )\""
+DEBUG = 1
+
+owner  = "Nobody"
+status = "new"
+queue  = "biotek-it-support"
+
+QUERY = "Owner=" + owner + " AND Status=" + status
+COMMAND = " ls "
+
+arguments = " -f id " + " -q " + queue + " "
+
+COMMAND = COMMAND + arguments
+
+QUERY_DEBUG="\'Owner=georgmar AND Status=open\'"
+
+
 TIME = 30 # in seconds
+TIME_DEBUG = 3 # in seconds
 KEEP_STATE = True
-RT_CLI = "/usr/bin/rt-3.6"
-_rt_cmd = [RT_CLI + " ls " + QUERY]
+RT_CLI = "/usr/bin/rt" # TODO: check the version of /usr/bin/rt to ensure it is at least 3.6
+#_rt_cmd = [RT_CLI + " ls " + QUERY]
+_rt_cmd = [RT_CLI + COMMAND + QUERY_DEBUG]
 _rt_img = "file://" + os.path.abspath(os.path.dirname(sys.argv[0])) + "/rt_img.png"
 
 seen_queue = []
@@ -25,44 +42,51 @@ def checkRT():
 
     global seen_queue, out_queue
 
+    if DEBUG: 
+    	print("QUERY: " + RT_CLI + COMMAND + QUERY_DEBUG)
     output = subprocess.Popen(_rt_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
     # Do nothing if it returns "No matching results."
     pat = "^No matching results."
-    match = re.match(pat, output)
+    empty_result = re.match(pat, output)
     
-    if not match:
+    if not empty_result:
         for line in output.split('\n'):
-            if len(line.strip()) == 0:
-                pass
-            
-            ticket_id = line.split(':')[0]
-            
-            if KEEP_STATE:
-                if ticket_id not in seen_queue:
-                    seen_queue.append(ticket_id)
-                    out_queue.append(line)
-                    # Keep the seen queue from growing too large (improve this)   
-                    if len(seen_queue) > 50:
-                        seen_queue.pop(0)
-                else:
-                    pass
-            else:
-                out_queue.append(line)
+            # ignore empty lines, or lines that do not start with a ticket number
+            if len(line.strip()) == 0 or re.match( "^Query", line ) or re.match( "^Ticket", line ) or re.match("^---", line) or re.match("^===", line ) :
+                continue
 
-        # Done for loop
-        # Display notice if there's something to show
-        if len(out_queue) != 0:
-            n = pynotify.Notification("RT Notice", '\n'.join(out_queue), _rt_img)
-            n.set_urgency(pynotify.URGENCY_LOW)
+            print line
+        sys.exit(0)
+
             
-            if not n.show():
-                print "Failed to send notification"
-                sys.exit(1)
+    #         ticket_id = line.split(':')[0]
+            
+    #         if KEEP_STATE:
+    #             if ticket_id not in seen_queue:
+    #                 seen_queue.append(ticket_id)
+    #                 out_queue.append(line)
+    #                 # Keep the seen queue from growing too large (improve this)   
+    #                 if len(seen_queue) > 50:
+    #                     seen_queue.pop(0)
+    #             else:
+    #                 pass
+    #         else:
+    #             out_queue.append(line)
+
+    #     # Done for loop
+    #     # Display notice if there's something to show
+    #     if len(out_queue) != 0:
+    #         n = pynotify.Notification("RT Notice", '\n'.join(out_queue), _rt_img)
+    #         n.set_urgency(pynotify.URGENCY_LOW)
+            
+    #         if not n.show():
+    #             print "Failed to send notification"
+    #             sys.exit(1)
                 
-            out_queue = []
+    #         out_queue = []
 
-    # So glib to keep calling me
+    # # So glib to keep calling me
     return True
 
 
@@ -73,7 +97,8 @@ if __name__ == '__main__':
 
     # Setup a loop and a callback function
     main = glib.MainLoop()
-    glib.timeout_add_seconds(TIME, checkRT)
+    # glib.timeout_add_seconds(TIME, checkRT)
+    glib.timeout_add_seconds(TIME_DEBUG, checkRT)
     main.run()
     
 
